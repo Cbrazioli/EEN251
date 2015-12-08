7958a995415a5e63c10e1c50894be6645d4c0bf0
Rafael Corsi <corsiferrao@gmail.com  git config --global user.name Your>
2015-12-08 08:48:14 -0200

    plano de ensino

---
+++ Exemplos/LEG_GPIO/GPIO/Debug/src/ASF/sam/utils/syscalls/gcc/syscalls.d
@@ -0,0 +1,64 @@
+src/ASF/sam/utils/syscalls/gcc/syscalls.d \
+ src/ASF/sam/utils/syscalls/gcc/syscalls.o: \
+ ../src/ASF/sam/utils/syscalls/gcc/syscalls.c \
+ /usr/arm-none-eabi/include/stdio.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.4/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/4.8.4/include/stdarg.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/types.h \
+ /usr/arm-none-eabi/include/machine/types.h \
+ /usr/arm-none-eabi/include/sys/stdio.h \
+ /usr/arm-none-eabi/include/sys/stat.h /usr/arm-none-eabi/include/time.h \
+ /usr/arm-none-eabi/include/machine/time.h
+
+/usr/arm-none-eabi/include/stdio.h:
+
+/usr/arm-none-eabi/include/_ansi.h:
+
+/usr/arm-none-eabi/include/newlib.h:
+
+/usr/arm-none-eabi/include/sys/config.h:
+
+/usr/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/lib/gcc/arm-none-eabi/4.8.4/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/4.8.4/include/stdarg.h:
+
+/usr/arm-none-eabi/include/sys/reent.h:
+
+/usr/arm-none-eabi/include/_ansi.h:
+
+/usr/arm-none-eabi/include/sys/_types.h:
+
+/usr/arm-none-eabi/include/machine/_types.h:
+
+/usr/arm-none-eabi/include/sys/lock.h:
+
+/usr/arm-none-eabi/include/sys/types.h:
+
+/usr/arm-none-eabi/include/machine/types.h:
+
+/usr/arm-none-eabi/include/sys/stdio.h:
+
+/usr/arm-none-eabi/include/sys/stat.h:
+
+/usr/arm-none-eabi/include/time.h:
+
+/usr/arm-none-eabi/include/machine/time.h:
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 EEN251 - Microcontroladores e Sistemas Embarcado
 ==================
 * Instituto Mauá de Tecnologia - www.maua.br 
@@ -18,3 +17,69 @@ Ser capaz de desenvolver, solucionar e otimizar necessidades e problemáticas en
 	* Interpretação de documentação (\textit{datasheet}, manual do usuário, esquemático)
 	* Compreender soluções empregadas em projetos existentes identificando suas vantagens e desvantagens
 	* Estruturação e gestão de projeto (código fonte, documentação)
+
+# Ementa
+
+ * Semana 01 - Introdução uC ARM
+    - Conteúdo
+        - Revisão uC
+        - Arquitetura ARM  
+        - Barramentos vs banco de registradores
+        - Periféricos 
+    - Avaliação
+        - Pesquisa sobre arquitetura ARM do processador utilizado SAM4S (AMBA, CortexM, Aplicações)
+        - Criar conta + repositorio GitHub
+ 
+ * Semana 02 - Revisão programaçao em C
+    - Conteúdo
+        - C -> OpCode
+        - Funções
+        - Ponteiros
+        - While(1){}
+    - Avaliação
+        - Exercícios
+
+ * Semana 03 - Introdução ambiente de desenvolvimento Atmel
+    - Conteúdo
+        - Criar um novo projeto
+        - Build
+        - Debug
+        - ASF
+        - IDE
+
+ * Semana 04 - GPIO (escrita e leitura)
+    - Conteúdo
+        - Revisão datasheet
+        - Periférico
+        - Funções ioport
+    - Avaliação
+        - Exercícios proposto em aula (modificação)
+        - Documentação no código
+
+ * Estrutura de SW (GPIO)
+ * Interrupção (GPIO)
+ * PWM
+ * TIMER
+ * 
+
+
+
+# Todo
+ - Criar tutorial Atmel Studio 7
+ - Criar repositório padrão para entrega de trabalho 
+
+
+
+
+ 12 - Prova
+
+ 19 - Prova
+ 20 - Prova   
+ 21
+ 22
+ 
+ 30
+ 
+ 39
+ 40
+ 41
